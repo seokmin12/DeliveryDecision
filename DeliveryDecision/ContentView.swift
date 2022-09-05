f312ac09286b044b9b3da28b855b4e8338ae0c77
이석민 <dltjrals13@naver.com>
2022-09-05 15:42:34 +0900

    Initial Commit

---
+++ DeliveryDecision/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeliveryDecision
+//
+//  Created by 이석민 on 2022/09/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeliveryDecision/DeliveryDecisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryDecisionApp.swift
+//  DeliveryDecision
+//
+//  Created by 이석민 on 2022/09/05.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryDecisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
